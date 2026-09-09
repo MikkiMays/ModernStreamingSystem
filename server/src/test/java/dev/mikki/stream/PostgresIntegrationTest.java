915aa130b1ace9bbd262b2852e81184a0bea014f
MikkiMays <653214@mail.ru>
2026-09-09 09:43:12 +0300

    Use canonical image digest syntax for PostgreSQL integration tests

--- server/src/test/java/dev/mikki/stream/PostgresIntegrationTest.java
+++ server/src/test/java/dev/mikki/stream/PostgresIntegrationTest.java
@@ -8,9 +8,10 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 @Testcontainers(disabledWithoutDocker = true)
 class PostgresIntegrationTest extends RoomServiceTest {
   @Container
+  // PostgreSQL 18.3 Alpine: use digest-only syntax supported by Testcontainers' parser.
   static PostgreSQLContainer postgres =
       new PostgreSQLContainer(
-          "postgres:18.3-alpine@sha256:54451ecb8ab38c24c3ec123f2fd501303a3a1856a5c66e98cecf2460d5e1e9d7");
+          "postgres@sha256:54451ecb8ab38c24c3ec123f2fd501303a3a1856a5c66e98cecf2460d5e1e9d7");
 
   @DynamicPropertySource
   static void database(DynamicPropertyRegistry registry) {
